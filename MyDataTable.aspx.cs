55a7bfca00772b8616d0df42fb7438dc8e650519
ImranGani <imrangani6@hotmail.com>
2014-07-08 16:36:49 +0530

    Revert "Testing"
    This reverts commit 80455e61ffaab6870a6f956afc20ec95a5a2bb6c.

--- MyDataTable.aspx.cs
+++ MyDataTable.aspx.cs
@@ -19,7 +19,7 @@ public partial class MyDataTable : System.Web.UI.Page
         // First Line
         // Bug Fixed
         // Dev1 Testing Line
-        // test 123
+        // test
     }
     private void myDataTable()
     {
